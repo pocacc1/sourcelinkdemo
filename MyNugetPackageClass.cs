cb95eabe5a9405e8204d3a777e9586ac50b2559b
fadil.sahin <fadil.sahin@piworks.net>
2023-07-12 07:04:26 +0300

    init

---
+++ MyNugetPackageClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DemoPackage
+{
+	public static class MyNugetPackageClass
+	{
+		public static string GetMyNugetPackageString()
+		{
+			return Get1();
+		}
+
+		private static string Get1()
+		{
+            return Get2();
+		}
+
+		public static string Get2()
+		{
+			return Get3();
+		}
+
+		public static string Get3()
+		{
+			return "This string is retrieved from my published nuget package.";
+		}
+	}
+}
